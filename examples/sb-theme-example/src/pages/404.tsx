d364cd082b532c33458cb4f114c9aaf0d9eebd0c
Anders Langseth <anderslangseth@gmail.com>
2023-09-22 19:33:47 +0200

    chore: not fond -> not found

--- examples/sb-theme-example/src/pages/404.tsx
+++ examples/sb-theme-example/src/pages/404.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const NotFond = () => {
+const NotFound = () => {
   return (
     <>
       <h1>404</h1>
     </>
-  )
-}
+  );
+};
 
-export default NotFond
+export default NotFound;
--- packages/gatsby-plugin-eufemia-theme-handler/themeHandler.js
+++ packages/gatsby-plugin-eufemia-theme-handler/themeHandler.js
@@ -1,107 +1,108 @@
-import React from 'react'
-import inlineScriptProd from '!raw-loader!terser-loader!./inlineScriptProd'
-import inlineScriptDev from '!raw-loader!terser-loader!./inlineScriptDev'
-import EventEmitter from './EventEmitter.js'
+import React from "react";
+import inlineScriptProd from "!raw-loader!terser-loader!./inlineScriptProd";
+import inlineScriptDev from "!raw-loader!terser-loader!./inlineScriptDev";
+import EventEmitter from "./EventEmitter.js";
 
-const defaultTheme = globalThis.EUFEMIA_THEME_defaultTheme || 'ui'
-const availableThemes = globalThis.EUFEMIA_THEME_themes || []
-const storageId = globalThis.EUFEMIA_THEME_storageId || 'eufemia-theme'
-const availableThemesArray = Object.keys(availableThemes)
+const defaultTheme = globalThis.EUFEMIA_THEME_defaultTheme || "ui";
+const availableThemes = globalThis.EUFEMIA_THEME_themes || [];
+const storageId = globalThis.EUFEMIA_THEME_storageId || "eufemia-theme";
+const availableThemesArray = Object.keys(availableThemes);
 
 export function getThemes() {
-  return availableThemes
+  return availableThemes;
 }
 
 export function isValidTheme(themeName) {
-  return availableThemesArray.includes(themeName)
+  return availableThemesArray.includes(themeName);
 }
 
 export function useTheme() {
-  const [theme, setTheme] = React.useState(getTheme)
+  const [theme, setTheme] = React.useState(getTheme);
 
   React.useEffect(() => {
-    const emitter = EventEmitter.createInstance('themeHandler')
+    const emitter = EventEmitter.createInstance("themeHandler");
     emitter.listen((theme) => {
-      setTheme(theme)
-    })
-  }, [])
+      setTheme(theme);
+    });
+  }, []);
 
-  return theme
+  return theme;
 }
 
 export function getTheme() {
-  if (typeof window === 'undefined') {
-    return defaultTheme
+  if (typeof window === "undefined") {
+    return defaultTheme;
   }
   try {
-    const data = window.localStorage.getItem(storageId)
-    const theme = JSON.parse(data?.startsWith('{') ? data : '{}')
+    const data = window.localStorage.getItem(storageId);
+    const theme = JSON.parse(data?.startsWith("{") ? data : "{}");
 
-    const regex = /.*eufemia-theme=([^&]*).*/
-    const query = window.location.search
-    const fromQuery = (regex.test(query) && query?.replace(regex, '$1')) || null
+    const regex = /.*eufemia-theme=([^&]*).*/;
+    const query = window.location.search;
+    const fromQuery =
+      (regex.test(query) && query?.replace(regex, "$1")) || null;
 
-    const themeName = fromQuery || theme?.name || defaultTheme
+    const themeName = fromQuery || theme?.name || defaultTheme;
 
     if (!isValidTheme(themeName)) {
-      console.error('Not valid themeName:', themeName)
-      return defaultTheme // stop here
+      console.error("Not valid themeName:", themeName);
+      return defaultTheme; // stop here
     }
 
-    return { ...theme, name: themeName }
+    return { ...theme, name: themeName };
   } catch (e) {
-    console.error(e)
-    return defaultTheme
+    console.error(e);
+    return defaultTheme;
   }
 }
 
 export function setTheme(themeProps, callback) {
-  const theme = { ...getTheme(), ...themeProps }
+  const theme = { ...getTheme(), ...themeProps };
 
   if (!isValidTheme(theme?.name)) {
-    console.error('Not valid themeName:', theme?.name)
-    return // stop here
+    console.error("Not valid themeName:", theme?.name);
+    return; // stop here
   }
 
   try {
     globalThis.__updateEufemiaThemeFile(theme.name, true, () => {
-      const emitter = EventEmitter.createInstance('themeHandler')
-      emitter.update(theme)
+      const emitter = EventEmitter.createInstance("themeHandler");
+      emitter.update(theme);
 
-      callback?.(theme)
-    })
+      callback?.(theme);
+    });
 
-    window.localStorage.setItem(storageId, JSON.stringify(theme))
+    window.localStorage.setItem(storageId, JSON.stringify(theme));
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 }
 
-let cachedHeadComponents = []
+let cachedHeadComponents = [];
 export const onPreRenderHTML = (
   { getHeadComponents, replaceHeadComponents },
   pluginOptions
 ) => {
-  let headComponents = getHeadComponents()
+  let headComponents = getHeadComponents();
 
-  const isDev = process.env.NODE_ENV !== 'production'
+  const isDev = process.env.NODE_ENV !== "production";
 
   // Make themes to not be embedded, but rather load as css files
   if (!isDev) {
-    let defaultElement
+    let defaultElement;
     for (const element of headComponents) {
-      const href = element.props['data-href']
-      if (href && href.includes('.css')) {
+      const href = element.props["data-href"];
+      if (href && href.includes(".css")) {
         if (
           availableThemesArray.some((name) => {
-            return href.includes(`/${name}.`)
+            return href.includes(`/${name}.`);
           })
         ) {
-          const themeName = (href.match(/\/([^.]*)\./) || [])?.[1]
+          const themeName = (href.match(/\/([^.]*)\./) || [])?.[1];
 
           // Collect all theme CSS file paths
           if (availableThemes[themeName]) {
-            availableThemes[themeName].file = href
+            availableThemes[themeName].file = href;
 
             // Store the default inline styles,
             // and place it below data-href="/commons.*.css"
@@ -109,25 +110,25 @@ export const onPreRenderHTML = (
               pluginOptions?.inlineDefaultTheme &&
               themeName === defaultTheme
             ) {
-              defaultElement = element
-              headComponents[element] = null
+              defaultElement = element;
+              headComponents[element] = null;
             } else {
               // Remove the inline style
               // but not when its the default theme
-              delete element.props['data-href']
-              delete element.props['data-identity']
-              delete element.props.dangerouslySetInnerHTML
+              delete element.props["data-href"];
+              delete element.props["data-identity"];
+              delete element.props.dangerouslySetInnerHTML;
             }
           }
         }
       }
     }
 
-    headComponents.push(defaultElement)
+    headComponents.push(defaultElement);
   } else {
     for (const key in availableThemes) {
-      availableThemes[key].isDev = true
-      availableThemes[key].file = `/${key}.css` // during dev, we get this file from the Webpack cache (not in /public)
+      availableThemes[key].isDev = true;
+      availableThemes[key].file = `/${key}.css`; // during dev, we get this file from the Webpack cache (not in /public)
     }
   }
 
@@ -136,12 +137,12 @@ export const onPreRenderHTML = (
    * and re-use on all other pages.
    */
   if (cachedHeadComponents?.length) {
-    headComponents.push(...cachedHeadComponents)
-    replaceComponents(headComponents)
-    return // stop here
+    headComponents.push(...cachedHeadComponents);
+    replaceComponents(headComponents);
+    return; // stop here
   }
 
-  cachedHeadComponents = []
+  cachedHeadComponents = [];
 
   cachedHeadComponents.push(
     <link
@@ -151,7 +152,7 @@ export const onPreRenderHTML = (
       type="text/css"
       as="style"
     />
-  )
+  );
 
   const replaceGlobalVars = (code) => {
     return code
@@ -162,8 +163,8 @@ export const onPreRenderHTML = (
       .replace(
         /globalThis.inlineDefaultTheme/g,
         JSON.stringify(pluginOptions?.inlineDefaultTheme)
-      )
-  }
+      );
+  };
 
   /**
    * NB: The dev script needs to be placed before the prod!
@@ -176,7 +177,7 @@ export const onPreRenderHTML = (
           __html: replaceGlobalVars(inlineScriptDev),
         }}
       />
-    )
+    );
   }
 
   cachedHeadComponents.push(
@@ -186,7 +187,7 @@ export const onPreRenderHTML = (
         __html: replaceGlobalVars(inlineScriptProd),
       }}
     />
-  )
+  );
 
   if (!pluginOptions?.inlineDefaultTheme) {
     cachedHeadComponents.push(
@@ -199,27 +200,27 @@ export const onPreRenderHTML = (
           href={availableThemes[defaultTheme].file}
         />
       </noscript>
-    )
+    );
   }
 
-  headComponents.push(...cachedHeadComponents)
+  headComponents.push(...cachedHeadComponents);
 
-  replaceComponents(headComponents)
+  replaceComponents(headComponents);
 
   function replaceComponents(headComponents) {
-    const uniqueHeadComponents = []
+    const uniqueHeadComponents = [];
 
     // Remove duplications
     headComponents.forEach((item) => {
       const exists = uniqueHeadComponents.some((i) => {
-        const href = i?.props?.['data-href']
-        return href && item?.props?.['data-href'] === href
-      })
+        const href = i?.props?.["data-href"];
+        return href && item?.props?.["data-href"] === href;
+      });
       if (!exists) {
-        uniqueHeadComponents.push(item)
+        uniqueHeadComponents.push(item);
       }
-    })
+    });
 
-    replaceHeadComponents(uniqueHeadComponents)
+    replaceHeadComponents(uniqueHeadComponents);
   }
-}
+};
